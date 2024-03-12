d57bdc2280d04d6bc77dde3025e150930189e212
Zeki BOY <zekiiboy@gmail.com>
2024-03-12 17:56:54 +0300

    HangfireJob

---
+++ HangfireApp/Jobs/Job.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace HangfireApp.Jobs
+{
+	public class Job
+	{
+
+
+		public Job()
+		{
+		}
+
+        public void DbControl()
+        {
+            //PROJEYİ ÇALIŞTIRDIĞIMDA MAİLİ GÖNDERDİ, CONTROLLER İÇİNDE ÇAĞIRMAMA GEREK KALMADI
+            SendEmail();
+        }
+
+        // public void SendEmail(string email, string subject, string body)
+        public void SendEmail()
+        {
+            SmtpClient smtp = new SmtpClient();
+
+            smtp.Host = "smtp.office365.com";
+            smtp.Port = 587;
+            //GMail SMTP sunucusuna erişeceğimiz Port numarasını belirtiyoruz.
+            smtp.EnableSsl = true;
+            //GMail SMTP sunucusuna bağlanacağımız protokolü belirliyoruz.True da https, False da http protokolü kullanılır.
+
+
+            NetworkCredential kullanicibilgi = new NetworkCredential("zekiiboy@hotmail.com", "marleylolo34");
+            //NetworkCredential tipinden bir nesne oluşturup, kullanıcı bilgilerimizi giriyoruz.
+            smtp.Credentials = kullanicibilgi;
+            //Bu kullanıcı bilgilerini Credentials ile ilgili SMTP bağlantısına atıyoruz.
+
+
+            MailAddress gonderen = new MailAddress("zekiiboy@hotmail.com", "Zeki Mail Test");
+            //Maili atacak kişinin adresi
+            MailAddress alici = new MailAddress("zekiiboy@gmail.com");
+            //Maili alacak kişinin adresini yazdık.
+            MailMessage mail = new MailMessage(gonderen, alici);
+            //MailMessage nesnemizi oluşturduk.MailAddress tipinden istediği gonderen ve alici nesnelerini, bu constructorından bağladık.
+            mail.Subject = "Mesajın Konusu";
+            mail.Body = "Mesajın İçeriği";
+            mail.IsBodyHtml = true;
+            //Mail'de html kod kullanılsın mı?True evet,False hayır.
+            smtp.Send(mail);
+            //Son olarak SmtpClient tipindeki smtp nesnemiz sayesinde, MailMessage tipindeki mail nesnemiz ilgili adrese, gonderen adıyla iletiliyor.
+        }
+
+    }
+}
+
---
+++ HangfireApp/Program.cs
@@ -0,0 +1,58 @@
+﻿
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Hangfire;
+using HangfireApp.Jobs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+
+//projenin çalışması için docker aktif olmalı
+var hangfireConnectionString = "Server=localhost;Database=HangfireTest;User Id=SA;Password=reallyStrongPwd123;";
+
+//var hangfireConnectionString = "Server=localhost; Database=HangfireTest; User Id=SA; Password=reallyStrongPwd123";
+
+builder.Services.AddHangfire(x =>
+{
+    x.UseSqlServerStorage(hangfireConnectionString);
+
+    //bu kısımda hata almamak için system.data.sqlClient veya microsoft.....SqlClient nugetleri şart
+    //crontab.cronhub.io üzerinden süre belirle
+    // RecurringJob.AddOrUpdate<Job>(j => j.DbControl(), "30 17  * * *");
+    RecurringJob.AddOrUpdate<Job>(j => j.DbControl(), "* * * * *");
+    //DbControl yerine direkt sendEmail metodunu kullan
+
+});
+
+builder.Services.AddHangfireServer();
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseHangfireDashboard();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
